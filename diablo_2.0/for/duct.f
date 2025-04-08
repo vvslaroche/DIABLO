198c4eed8b51b688a1fe270b2f3e5234bc78dad0
Vincent Laroche <vvslaroche@gmail.com>
2025-04-08 15:05:44 -0700

    Adding initialization
    Add CREATE_GRID_DUCT, INPUT_DUCT, and INIT_DUCT_MOVIE

--- diablo_2.0/for/duct.f
+++ diablo_2.0/for/duct.f
@@ -46,83 +46,6 @@ C----*|--.---------.---------.---------.---------.---------.---------.-|-------|
       RETURN
       END
 
-C----*|--.---------.---------.---------.---------.---------.---------.-|-------|
-      SUBROUTINE CREATE_GRID_DUCT_OLD
-C----*|--.---------.---------.---------.---------.---------.---------.-|-------|
-      include 'header'
-      integer I,J,K
-
-               WRITE (6,*) 'Fourier in X'
-         DO I=0,NX
-           GX(I)=(I*LX)/NX
-           DX(I)=LX/NX
-           IF (VERBOSITY .GT. 3) WRITE(6,*) 'GX(',I,') = ',GX(I)
-         END DO
-         WRITE (6,*) 'Finite-difference in Z'
-         OPEN (30,file='zgrid.txt',form='formatted',status='old')
-         READ (30,*) NZ_T
-C Check to make sure that grid file is the correct dimensions
-         IF (NZ_T.ne.NZ) THEN
-           WRITE(6,*) 'NZ, NZ_T',NZ,NZ_T
-           STOP 'Error: zgrid.txt wrong dimensions'
-         END IF
-         DO K=1,NZ+1
-           READ(30,*) GZ(k)
-           IF (VERBOSITY .GT. 3) WRITE(6,*) 'GZ(',K,') = ',GZ(K)
-         END DO
-         DO K=1,NZ
-           READ(30,*) GZF(k)
-           IF (VERBOSITY .GT. 3) WRITE(6,*) 'GZF(',K,') = ',GZF(K)
-         END DO
-C Define ghost cells, if needed for this grid...
-         GZF(0)=2.d0*GZF(1)-GZF(2)
-         GZF(NZ+1)=2.d0*GZF(NZ)-GZF(NZM)
-         GZ(0)=2.d0*GZ(1)-GZ(2)
-C Define grid spacing
-         DO K=1,NZ+1
-           DZ(K)=(GZF(K)-GZF(K-1))
-         END DO
-         DO K=1,NZ
-           DZF(K)=(GZ(K+1)-GZ(K))
-         END DO
-         CLOSE(30)
-         WRITE (6,*) 'Finite-difference in Y'
-         OPEN (30,file='./ygrid.txt',form='formatted',status='old')
-         READ (30,*) NY_T
-C Check to make sure that grid file is the correct dimensions
-         IF (NY_T.ne.NY) THEN
-           WRITE(6,*) 'NY, NY_T',NY,NY_T
-           STOP 'Error: ygrid.txt wrong dimensions'
-         END IF
-         DO J=1,NY+1
-           READ(30,*) GY(j)
-           IF (VERBOSITY .GT. 3) WRITE(6,*) 'GY(',J,') = ',GY(J)
-         END DO
-         DO J=1,NY
-           READ(30,*) GYF(j)
-           IF (VERBOSITY .GT. 3) WRITE(6,*) 'GYF(',J,') = ',GYF(J)
-         END DO
-         CLOSE(30)
-
-         IF (USE_MPI) THEN
-           CALL GHOST_GRID_MPI
-         ELSE
-C Define ghost cells
-           GYF(0)=2.d0*GYF(1)-GYF(2)
-           GYF(NY+1)=2.d0*GYF(NY)-GYF(NYM)
-           GY(0)=2.d0*GY(1)-GY(2)
-         END IF
-
-C Define grid spacing
-         DO J=1,NY+1
-           DY(J)=(GYF(J)-GYF(J-1))
-         END DO
-         DO J=1,NY
-           DYF(J)=(GY(J+1)-GY(J))
-         END DO
-       RETURN
-       END
-
 
 C----*|--.---------.---------.---------.---------.---------.---------.-|-------|
       SUBROUTINE CREATE_GRID_DUCT
@@ -153,11 +76,19 @@ C----*|--.---------.---------.---------.---------.---------.---------.-|-------|
          end if
 
          call ReadGridHDF5(FNAME,2)
+         DO J=0,NY+1
+           IF (VERBOSITY .GT. 3 .AND. RANKZ.EQ.0)
+     &          WRITE(6,*) 'RANKY=',RANKY,'GYF(',J,') = ',GYF(J)
+         END DO
 
          call ReadGridHDF5(FNAME,3)
+         DO K=0,NZP+1
+           IF (VERBOSITY .GT. 3 .AND. RANKY.EQ.0)
+     &          WRITE(6,*) 'RANKZ=',RANKZ,'GZFP(',K,') = ',GZFP(K)
+         END DO
 
 
-C Define grid spacing
+C Define grid spacing - Y
          DO J=1,NY+1
            DY(J)=(GYF(J)-GYF(J-1))
          END DO
@@ -165,14 +96,14 @@ C Define grid spacing
            DYF(J)=(GY(J+1)-GY(J))
          END DO
          DYF(NY+1)=DYF(NY)
-C Define grid spacing
-         DO K=1,NZ+1
-           DZ(K)=(GZF(K)-GZF(K-1))
+C Define grid spacing - Z
+         DO K=1,NZP+1
+           DZP(K)=(GZFP(K)-GZFP(K-1))
          END DO
-         DO K=1,NZ
-           DZF(K)=(GZ(K+1)-GZ(K))
+         DO K=1,NZP
+           DZFP(K)=(GZP(K+1)-GZP(K))
          END DO
-         DZF(NZ+1)=DZF(NZ)
+         DZFP(NZP+1)=DZFP(NZP)
 
          RETURN
          END
@@ -180,14 +111,130 @@ C Define grid spacing
 C----*|--.---------.---------.---------.---------.---------.---------.-|-------|
       SUBROUTINE INPUT_DUCT
 C----*|--.---------.---------.---------.---------.---------.---------.-|-------|
-       RETURN
-       END
+      INCLUDE 'header'
+      REAL    VERSION, CURRENT_VERSION
+      INTEGER I,J,K,N
+
+! Read in input parameters specific for duct flow case
+      OPEN (11,file='input_duct.dat',form='formatted',status='old')
+C Read input file.
+
+      CURRENT_VERSION=2.0
+      READ(11,*)
+      READ(11,*)
+      READ(11,*)
+      READ(11,*)
+      READ(11,*) VERSION
+      IF (VERSION .NE. CURRENT_VERSION)
+     &         STOP 'Wrong input data format input_duct'
+      READ(11,*)
+      READ(11,*) TIME_AD_METH
+      READ(11,*)
+      READ(11,*) LES_MODEL_TYPE
+      READ(11,*)
+      READ(11,*) IC_TYPE, KICK
+      READ(11,*)
+      READ(11,*) I_RO
+      READ(11,*)
+      READ(11,*) GRAV_X, GRAV_Y, GRAV_Z
+      READ(11,*)
+      READ(11,*) F_TYPE, UBULK0, PX0, OMEGA0, AMP_OMEGA0
+      READ(11,*)
+      READ(11,*) U_BC_YMIN, U_BC_YMIN_C1, U_BC_YMIN_C2, U_BC_YMIN_C3
+      READ(11,*)
+      READ(11,*) V_BC_YMIN, V_BC_YMIN_C1, V_BC_YMIN_C2, V_BC_YMIN_C3
+      READ(11,*)
+      READ(11,*) W_BC_YMIN, W_BC_YMIN_C1, W_BC_YMIN_C2, W_BC_YMIN_C3
+      READ(11,*)
+      READ(11,*) U_BC_YMAX, U_BC_YMAX_C1, U_BC_YMAX_C2, U_BC_YMAX_C3
+      READ(11,*)
+      READ(11,*) V_BC_YMAX, V_BC_YMAX_C1, V_BC_YMAX_C2, V_BC_YMAX_C3
+      READ(11,*)
+      READ(11,*) W_BC_YMAX, W_BC_YMAX_C1, W_BC_YMAX_C2, W_BC_YMAX_C3
+      READ(11,*)
+      READ(11,*) U_BC_ZMIN, U_BC_ZMIN_C1, U_BC_ZMIN_C2, U_BC_ZMIN_C3
+      READ(11,*)
+      READ(11,*) V_BC_ZMIN, V_BC_ZMIN_C1, V_BC_ZMIN_C2, V_BC_ZMIN_C3
+      READ(11,*)
+      READ(11,*) W_BC_ZMIN, W_BC_ZMIN_C1, W_BC_ZMIN_C2, W_BC_ZMIN_C3
+      READ(11,*)
+      READ(11,*) U_BC_ZMAX, U_BC_ZMAX_C1, U_BC_ZMAX_C2, U_BC_ZMAX_C3
+      READ(11,*)
+      READ(11,*) V_BC_ZMAX, V_BC_ZMAX_C1, V_BC_ZMAX_C2, V_BC_ZMAX_C3
+      READ(11,*)
+      READ(11,*) W_BC_ZMAX, W_BC_ZMAX_C1, W_BC_ZMAX_C2, W_BC_ZMAX_C3
+      READ(11,*)
+! Read in boundary conditions and background gradients for the N_TH scalars
+      DO N=1,N_TH
+        READ(11,*)
+        READ(11,*) TH_BC_YMIN(N),TH_BC_YMIN_C1(N),TH_BC_YMIN_C2(N)
+     &             ,TH_BC_YMIN_C3(N)
+        READ(11,*)
+        READ(11,*) TH_BC_YMAX(N),TH_BC_YMAX_C1(N),TH_BC_YMAX_C2(N)
+     &             ,TH_BC_YMAX_C3(N)
+        READ(11,*)
+        READ(11,*) TH_BC_ZMIN(N),TH_BC_ZMIN_C1(N),TH_BC_ZMIN_C2(N)
+     &             ,TH_BC_ZMIN_C3(N)
+        READ(11,*)
+        READ(11,*) TH_BC_ZMAX(N),TH_BC_ZMAX_C1(N),TH_BC_ZMAX_C2(N)
+     &             ,TH_BC_ZMAX_C3(N)
+        READ(11,*)
+        READ(11,*) DRHODX(N), DRHODZ(N)
+      END DO
+
+      RETURN
+      END
 
 C----*|--.---------.---------.---------.---------.---------.---------.-|-------|
       SUBROUTINE INIT_DUCT_MOVIE
 C----*|--.---------.---------.---------.---------.---------.---------.-|-------|
-       RETURN
-       END
+      INCLUDE 'header'
+      CHARACTER*55 FNAME
+      INTEGER I,J,K
+
+! Set parameters for writing "movie" files with 2d slices
+
+! First, read in the center of each 2D slice
+      OPEN(unit=650,file='MOVIE.dat',status='old',
+     &           form='formatted')
+      READ(650, *) XcMovie, YcMovie, ZcMovie
+      CLOSE(650)
+
+! Get the indices
+      NxMovie=int(XcMovie*NX/LX)
+
+      RankYMovie=-1
+      IF (GYF(JSTART).LE.YcMovie.and.GYF(JEND+1).GT.YcMovie) THEN
+         RankYMovie=RANKY
+         I=1
+         DO WHILE (.not.(GYF(I).LE.YcMovie .and. GYF(I+1).GT.YcMovie))
+            I=I+1
+         END DO
+         NyMovie=I;
+      END IF
+
+      RankZMovie=-1
+      IF (GZFP(KSTART).LE.ZcMovie.and.GZFP(KEND+1).GT.ZcMovie) THEN
+         RankZMovie=RANKZ
+         I=1
+         DO WHILE (.not.(GZFP(I).LE.ZcMovie .and. GZFP(I+1).GT.ZcMovie))
+            I=I+1
+         END DO
+         NzMovie=I;
+      END IF
+
+      IF (RANKY.eq.RankYMovie .and. RANKZ.eq.RankZMovie) THEN
+         write(*,*) 'Movie Parameters, RANK:', RANK
+         write(*,*) '    Xc: ', GX(NxMovie),
+     &      ' (NxMovie: ', NxMovie, ')'
+         write(*,*) '    Yc: ', GYF(NyMovie),
+     &      ' (NyMovie: ', NyMovie, ')'
+         write(*,*) '    Zc: ', GZFP(NzMovie),
+     &      ' (NzMovie: ', NzMovie, ')'
+      END IF
+
+      RETURN
+      END
 
 C----*|--.---------.---------.---------.---------.---------.---------.-|-------|
       SUBROUTINE SAVE_STATS_DUCT(FINAL)
